0179be57543cad8b894906614ae5d75592750f51
MinsooCha <65316983+MinsooCha@users.noreply.github.com>
2024-08-28 16:53:39 +0900

    Add files via upload

---
+++ VM_Clean_삭제_명령어.ps1
@@ -0,0 +1,14 @@
+﻿asnp citrix*
+
+$Path="C:\temp\info.csv"
+Get-BrokerMachine -DesktopGroupName "Static Dedi" | Select-Object MachineName, DesktopGroupName, HostedMachineName, CatalogName | Export-csv -Path $Path -Encoding UTF8 
+$VMList = Import-Csv -Path $Path
+
+foreach ($VMInfo in $VMList) {
+    $ADSID = Get-ProvVM -VMName $VMInfo.HostedMachineName | Select-Object ADAccountSid
+    Get-BrokerMachine -MachineName $VMInfo.MachineName | Remove-BrokerMachine -DesktopGroup $VMInfo.DesktopGroupName
+    Get-ProvVM -VMName $VMInfo.HostedMachineName | Unlock-ProvVM
+    Get-ProvVM -VMName $VMInfo.HostedMachineName | Remove-ProvVM
+    Get-BrokerMachine $VMInfo.MachineName | Remove-BrokerMachine
+    Remove-AcctADAccount -ADAccountSid $ADSID.ADAccountSid -IdentityPoolName $VMinfo.CatalogName -RemovalOption "delete"
+}
